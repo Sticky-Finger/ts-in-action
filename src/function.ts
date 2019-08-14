805df0a93f31335d39334af63d133eaa3def7540
jojo <wpwwanpengwang@163.com>
2019-08-15 01:19:47 +0800

    feat: 函数

---
+++ src/function.ts
@@ -0,0 +1,46 @@
+// 函数定义
+function add1(x: number, y: number) {
+  return x + y;
+}
+
+let add2: (x: number, y: number) => number;
+
+type add3 = (x: number, y: number) => number;
+
+interface add4 {
+  (x: number, y: number): number;
+}
+
+// 函数参数可选
+function add5(x: number, y?: number) { // 可选参数必须位于必选参数之后
+  return y ? x + y : x;
+}
+add5(1);
+
+// 参数设置默认参数
+function add6(x: number, y = 0, z: number, q = 1) {
+  return x + y + z + q;
+}
+console.log(add6(1, undefined, 3));
+
+function add7(x: number, ...rest: number[]) {
+  return x + rest.reduce((pre, cur) => pre + cur);
+}
+console.log(add7(1, 2, 3, 4, 5));
+
+/**
+ * 函数重载
+ */
+function add8(...rest: number[]): number;
+function add8(...rest: string[]): string;
+function add8(...rest: any[]): any {
+  let first = rest[0];
+  if (typeof first === 'string') {
+    return rest.join('');
+  }
+  if (typeof first === 'number') {
+    return rest.reduce((pre, cur) => pre + cur);
+  }
+}
+console.log(add8(1, 3, 3));
+console.log(add8('a', 'b', 'c'));
